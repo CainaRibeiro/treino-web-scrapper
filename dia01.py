4d95b7d12be836d04611d35c679141daf80c3191
Cainã Martins Teixeira Ribeiro <cainaribeiro.it@outlook.com>
2022-11-01 12:24:46 -0300

    -

---
+++ dia01.py
@@ -0,0 +1,13 @@
+#Biblioteca para pegar informações de uma URL
+#O python é capaz de ler apenas o único arquivo que você requisitou
+
+
+from urllib.request import urlopen
+
+#Ler https://docs.python.org/3/library/urllib.html, documento sobre a biblioteca urllib
+
+#Instalando a biblioteca BeautifulSoup
+#Usaremos a biblioteca BeautifulSoup 4 ou BS4
+
+#from bs4 import BeautifulSoup em um terminal python
+
